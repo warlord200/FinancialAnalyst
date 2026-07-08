4ef343e46d9168c91ba8afbc1a684e15be5fb908
ppan0039 <ppan0039@student.monash.edu>
2026-07-08 23:13:07 +0800

    Updated to accomodate multi files

--- app.py
+++ app.py
@@ -1,31 +1,49 @@
-import os
 import gradio as gr
 import helper
-import asyncio
-import chromadb
 
-from llama_index.readers.sec_filings import SECFilingsLoader
-from llama_index.core import Settings, VectorStoreIndex, SimpleDirectoryReader, Document, StorageContext, SummaryIndex
-from llama_index.core.tools import QueryEngineTool, ToolMetadata
+# from llama_index.readers.sec_filings import SECFilingsLoader
+from llama_index.core import (
+    Settings,
+    VectorStoreIndex,
+    # SimpleDirectoryReader,
+    Document,
+    # StorageContext,
+    # SummaryIndex,
+)
+
+# from llama_index.core.tools import QueryEngineTool, ToolMetadata
 from llama_index.core.agent import ReActAgent
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.deepseek import DeepSeek
 from llama_index.core.readers.base import BaseReader
-from llama_index.vector_stores.chroma import ChromaVectorStore
+
+# from llama_index.vector_stores.chroma import ChromaVectorStore
 from liteparse import LiteParse
 from typing import Dict
-from utils import CustomDocs
+from CustomDocs import CustomDocs
+from FileReader import FileReader
+from llama_index.core.objects import ObjectIndex
 
 
 class LiteParseReader(BaseReader):
     def load_data(self, file_path: str, extra_info=None):
-        parser = LiteParse(
-            output_format="markdown"
-        )
+        parser = LiteParse(output_format="markdown")
 
         result = parser.parse(file_path)
 
-        return [Document(text=result.text, extra_info=extra_info or {})]
+        base_info = extra_info or {}
+        documents = []
+
+        for page in result.pages:
+            page_info = {
+                **base_info,
+                "page_label": str(page.page_num),
+                "page_number": page.page_num,
+            }
+            documents.append(Document(text=page.text, extra_info=page_info))
+
+        return documents
+
 
 file_extractor: Dict[str, BaseReader] = {".pdf": LiteParseReader()}
 
@@ -33,10 +51,9 @@ print("Initializing LLM and embed model...")
 embed_model = HuggingFaceEmbedding(model_name="codefuse-ai/F2LLM-v2-80M")
 llm = DeepSeek(model="deepseek-v4-flash", api_key=helper.get_deepseek_api_key())
 
-# Set globally 
+# Set globally
 Settings.embed_model = embed_model
 Settings.llm = llm
-
 
 # 2. Data Ingestion
 print("Downloading SEC fillings...")
@@ -45,21 +62,38 @@ print("Downloading SEC fillings...")
 
 print("Loading documents into memory...")
 
-tesla = CustomDocs("Tesla", "data/2026","Tesla company",file_extractor)
+list_getter = FileReader("./data")
+names_dir_list = list_getter.get_fnames_and_dir()
 
-tesla_tools = tesla.get_tools()
+all_tools = []
+
+for company_name, company_file_dir in names_dir_list:
+    ctx_desc = company_name + " company"
+    company = CustomDocs(company_name, company_file_dir, ctx_desc, file_extractor)
+    company_tools = company.get_tools()
+    all_tools.extend(company_tools)
+
+
+obj_index = ObjectIndex.from_objects(
+    all_tools,
+    index_cls=VectorStoreIndex,
+)
+
+obj_retriever = obj_index.as_retriever(similarity_top_k=3)
 
 # 5. Agent Initialization
 agent = ReActAgent(
-    tools=tesla_tools, 
-    llm=llm, 
-    verbose=True
+    tool_retriever=obj_retriever,
+    llm=llm,
+    verbose=True,
 )
+
 
 # 6. UI Setup
 async def chat_with_agent(message: str, history):
     result = await agent.run(user_msg=message)
     return str(result)
+
 
 print("Launching UI...")
 demo = gr.ChatInterface(
